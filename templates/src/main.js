c6bb0f58fed1bc4a1d3a6e086c46992299c13456
kamalasaurus <kamalasaurus@gmail.com>
2018-01-28 20:22:13 -0500

    app entry

--- templates/src/main.js
+++ templates/src/main.js
@@ -1,7 +1,11 @@
+import App from './js/App.js';
+
 void function() {
 
   if ('serviceWorker' in navigator)
     navigator.serviceWorker.register('service-worker.js', { scope: '/' });
+
+  new App(document.getElementById('app'));
 
 }();
 
