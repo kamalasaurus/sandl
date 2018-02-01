3bd82e170e536d4c6efd71e5fd4ed60457b88092
kamalasaurus <kamalasaurus@gmail.com>
2018-02-01 20:48:38 +0000

    typo

--- skeleton/src/app/App.js
+++ skeleton/src/app/App.js
@@ -2,7 +2,7 @@ import mithril from './node_modules/mithril/mithril.js';
 
 export default function App() {
   const div = document.createElement('div');
-  div.innerHTML = 'Hello beachside world!"
+  div.innerHTML = 'Hello beachside world!';
   document.body.appendChild(div);
 };
 
