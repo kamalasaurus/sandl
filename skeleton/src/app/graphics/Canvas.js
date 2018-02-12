176908127204d13b4794f64990ee68dea6ffc1ef
kamalasaurus <kamalasaurus@gmail.com>
2018-02-12 22:14:17 +0000

    updates to the canvas

--- skeleton/src/app/graphics/Canvas.js
+++ skeleton/src/app/graphics/Canvas.js
@@ -3,6 +3,9 @@ import  m from '../../../node_modules/mithril/mithril.js'
 export default class Canvas {
 
   constructor() {
+    // create css parser script to make it a loadable json for modular height/width
+    // create script to generate module that has default css
+    this.canvas = m('canvas', {id: 'canvas', class: 'canvas'});
     // assign canvas to vnode somehow
     // maybe have the fetch for the glsl here
     // instead of oninit?
