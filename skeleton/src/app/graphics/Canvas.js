18bfe8f9733493eaab85542a79da162717179fc7
kamalasaurus <kamalasaurus@gmail.com>
2018-03-27 20:12:09 -0700

    initial synchrony demo

--- skeleton/src/app/graphics/Canvas.js
+++ skeleton/src/app/graphics/Canvas.js
@@ -31,6 +31,7 @@ export default class Canvas {
           });
 
           gl.linkProgram(program);
+          // classname or super.initialize?
           this.initialize(gl, program);
         };
     };
@@ -40,11 +41,84 @@ export default class Canvas {
     };
   }
 
+  createRectangles(gl, params, index, time) {
+    let primitiveType = gl.TRIANGLES;
+    let offset = 0;
+    let count = 6;
+
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+
+    gl.uniform1f(params.col, time);
+    gl.uniform1f(params.colPos, index);
+    gl.uniform1f(params.scale, time);
+    gl.uniform1f(params.transloc, index);
+
+    gl.drawArrays(primitiveType, offset, count);
+  }
+
+  update(gl, params, time) {
+    for (i = 100; i < 100; i++) {
+      this.createRectangles(gl, params, i, time || 0);
+    }
+    requestAnimationFrame(update.bind(this, gl, params));
+  }
+
   initialize(gl, program) {
     //TODO:!!
-    //after this, only the wasm loading :)
-    //then publish as npm module :)
-    //build out the program here!!
+    // publish as npm module
+    // after this, only the wasm loading :)
+
+    let buf = gl.createBuffer();
+    let loc = gl.getAttribLocatoin(program, 'a_position');
+
+    let params = {
+      transloc: gl.getUniformLocation(program, 'u_translation');
+      uni: gl.getUniformLocation(program, 'u_resolution');
+      col: gl.getUniformLocation(program, 'u_color');
+      scale: gl.getUniformLocation(program, 'u_scale');
+      colPos: gl.getUniformLocation(program, 'u_color_position');
+    };
+
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+    gl.clearColor(0, 0, 0, 0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    gl.useProgram(program);
+    gl.uniform2f(uni, gl.canvas.width, gl.canvas.height);
+    gl.enableVertexAttribArray(loc);
+    gl.bindBuffer(gl.ARRAY_BUFFER_B, buf);
+
+    let size = 2;
+    let type = gl.FLOAT;
+    let normalize = false;
+    let stride = 0;
+    let offset = 0;
+
+    gl.vertexAttribPointer(
+      loc,
+      size,
+      type,
+      normalize,
+      stride,
+      offset
+    );
+
+    let x1 = 10;
+    let x2 = x1 + 10;
+    let y1 = 10;
+    let y2 = y1 + 50;
+
+    params.positions = [
+      x1, y1,
+      x2, y1,
+      x1, y2,
+      x1, y2,
+      x2, y1,
+      x2, y2
+    ];
+
+    this.update(gl, params);
   }
 
   getshaders({vert, frag}) {
